9cc4f68b16f58d02dc34bd34d1d3c53bbac402e2
deniskristak <denis.kristak@gmail.com>
2023-02-14 08:36:54 +0100

    added test script for scib

---
+++ 33_scib_pipeline/test-scib-pipeline.sh
@@ -0,0 +1,9 @@
+ml load scib-pipeline &&
+
+cd $EBROOTSCIBMINPIPELINE/pipeline/ &&
+
+python data/generate_data.py &&
+
+snakemake --configfile configs/test_data-R4.0.yaml -n &&
+
+snakemake --configfile configs/test_data-R4.0.yaml --cores 8 > test_pipeline.log 2>&1 &&
