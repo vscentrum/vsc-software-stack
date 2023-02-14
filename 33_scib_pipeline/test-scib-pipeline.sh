a869bb2fb3524876115cacae87187f59388cbffd
deniskristak <denis.kristak@gmail.com>
2023-02-14 10:35:27 +0100

    newer version of scib - should fix the louvain issue

--- 33_scib_pipeline/test-scib-pipeline.sh
+++ 33_scib_pipeline/test-scib-pipeline.sh
@@ -6,4 +6,4 @@ python data/generate_data.py &&
 
 snakemake --configfile configs/test_data-R4.0.yaml -n &&
 
-snakemake --configfile configs/test_data-R4.0.yaml --cores 8 > test_pipeline.log 2>&1 &&
+snakemake --configfile configs/test_data-R4.0.yaml --cores 8 > test_pipeline.log 2>&1
