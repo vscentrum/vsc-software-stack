825411afac75f0022a87c5f0588896d160a60f9e
Alex Domingo <alex.domingo.toro@vub.be>
2024-04-22 16:36:54 +0200

    add Highway v1.0.5 for GCCcore/12.3.0

---
+++ easyconfigs/h/Highway/.modulerc.lua
@@ -0,0 +1,6 @@
+local cpuarch=os.getenv("VSC_ARCH_LOCAL") or ""
+if ( cpuarch == "zen4") then
+    -- Automatically swap broken versions of Highway on zen4
+    -- see https://github.com/google/highway/issues/1913
+    module_version("Highway/1.0.5-GCCcore-12.3.0", "1.0.4-GCCcore-12.3.0")
+end
