c1c7385406ebb2ba85d40375738aad08ce09832c
Alex Domingo <alex.domingo.toro@vub.be>
2024-06-26 15:17:38 +0200

    add sane easyconfig for libjxl v0.8.1 on 2022a

--- easyconfigs/h/Highway/.modulerc.lua
+++ easyconfigs/h/Highway/.modulerc.lua
@@ -3,5 +3,4 @@ if ( cpuarch == "zen4") then
     -- Automatically swap broken versions of Highway on zen4
     -- see https://github.com/google/highway/issues/1913
     module_version("Highway/1.0.5-GCCcore-12.3.0", "1.0.4-GCCcore-12.3.0")
-    module_version("Highway/1.0.3-GCCcore-11.3.0", "1.0.4-GCCcore-11.3.0")
 end
